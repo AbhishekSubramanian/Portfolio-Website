97dcdf5ab943e9d1238a9505cd28bf19d9d5cb0b
AbhishekSubramanian <abhisheksubramanian23@gmail.com>
2024-09-26 04:25:10 -0400

    Added lorem ipsum to the about me section

--- src/sections/aboutme.js
+++ src/sections/aboutme.js
@@ -6,7 +6,7 @@ const AboutMe = () => {
     <Box id="aboutme">
       <Typography variant="h4">About Me</Typography>
       <Typography mt={2}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. I am a skilled software developer with a passion for machine learning and full-stack development. Currently pursuing my master's in Computer Engineering at Virginia Tech.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit
       </Typography>
     </Box>
   );
